bca15e42e2306cda4bc34a998f73c3b8728f375b
Fernando da Silva Filho <fernando.filho@MMB033736SPO.local>
2023-05-04 20:44:35 -0300

    incluindo testes de API e alteracao de pastas

---
+++ api/spec/post_session_spec.rb
@@ -0,0 +1,26 @@
+require "httparty"
+
+describe "POST /sessions" do
+  context "login com sucesso" do
+    before(:all) do
+      payload = { email: "betao@yahoo.com", password: "pwd123" }
+
+      @result = HTTParty.post(
+        "http://rocklov-api:3333/sessions",
+        body: payload.to_json,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      )
+     
+    end
+    
+    it "valida status code" do
+      expect(@result.code).to eql 200
+    end
+
+    it "valida id do usuário" do
+      expect(@result.parsed_response["_id"].length).to eql 24
+    end
+  end
+end
