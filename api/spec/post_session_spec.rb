04d04dba4f4b9dd1a2102e8d6c9057283c3ff54d
Fernando da Silva Filho <fernando.filho@MMB033736SPO.local>
2023-05-08 13:56:20 -0300

    Aula teste orientado a dados

--- api/spec/post_session_spec.rb
+++ api/spec/post_session_spec.rb
@@ -16,18 +16,58 @@ describe "POST /sessions" do
     end
   end
 
-  context "senha invalida" do
-    before(:all) do
-      payload = { email: "betao@yahoo.com", password: "123456" }
-      @result = Sessions.new.login(payload)
-    end
+  examples = [
+    {
+      title: "senha incorreta",
+      payload: { email: "betao@yahoo.com", password: "123456" },
+      code: 401,
+      error: "Unauthorized",
+    },
+    {
+      title: "usuario não existe",
+      payload: { email: "404@yahoo.com", password: "123456" },
+      code: 401,
+      error: "Unauthorized",
+    },
+    {
+      title: "email em branco",
+      payload: { email: "", password: "123456" },
+      code: 412,
+      error: "required email",
+    },
+    {
+      title: "sem o campo email",
+      payload: { password: "123456" },
+      code: 412,
+      error: "required email",
+    },
+    {
+      title: "senha em branco",
+      payload: { email: "betao@yahoo.com", password: "" },
+      code: 412,
+      error: "required password",
+    },
+    {
+      title: "sem o campo senha",
+      payload: { email: "betao@yahoo.com" },
+      code: 412,
+      error: "required password",
+    },
+  ]
 
-    it "valida status code" do
-      expect(@result.code).to eql 401
-    end
+  examples.each do |e|
+    context "#{e[:title]}" do
+      before(:all) do
+        @result = Sessions.new.login(e[:payload])
+      end
 
-    it "valida id do usuário" do
-      expect(@result.parsed_response["error"]).to eql "Unauthorized"
+      it "valida status code #{e[:code]}" do
+        expect(@result.code).to eql e[:code]
+      end
+
+      it "valida id do usuário" do
+        expect(@result.parsed_response["error"]).to eql e[:error]
+      end
     end
   end
 end
