fd0a50fab5636485e7744fde7c3ece0fb8d2acbf
Fernando da Silva Filho <fernando.filho@MMB033736SPO.local>
2023-05-08 08:03:20 -0300

    criação da pasta routes

--- api/spec/post_session_spec.rb
+++ api/spec/post_session_spec.rb
@@ -1,17 +1,9 @@
-require "httparty"
+require_relative "routes/sessions"
 
 describe "POST /sessions" do
   context "login com sucesso" do
     before(:all) do
-      payload = { email: "betao@yahoo.com", password: "pwd123" }
-
-      @result = HTTParty.post(
-        "http://rocklov-api:3333/sessions",
-        body: payload.to_json,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      )
+        @result = Sessions.new.login("betao@yahoo.com", "pwd123")
      
     end
     
@@ -21,6 +13,21 @@ describe "POST /sessions" do
 
     it "valida id do usuário" do
       expect(@result.parsed_response["_id"].length).to eql 24
+    end
+  end
+
+  context "senha invalida" do
+    before(:all) do
+        @result = Sessions.new.login("betao@yahoo.com", "123456")
+     
+    end
+    
+    it "valida status code" do
+      expect(@result.code).to eql 401
+    end
+
+    it "valida id do usuário" do
+      expect(@result.parsed_response["error"]).to eql "Unauthorized"
     end
   end
 end
